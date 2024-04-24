e039b3b48bb93378e41c555d1ecbea34ba459dfe
Camille Crapat <camille.capt@gmail.com>
2024-04-24 11:56:59 +0200

    Initialisation du projet

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode></React.StrictMode>);
