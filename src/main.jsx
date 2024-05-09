502abe20e3d4910899da71c15ad2e00b2bf0f30c
Camille Crapat <camille.capt@gmail.com>
2024-05-09 12:09:37 +0200

    Création du main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './global.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode></React.StrictMode>);
+import Home from './pages/Home';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Error from './pages/Error';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<SideBar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
+);
