55dc1634620807cb7e45acc553005c3df4c26aae
Camille Crapat <camille.capt@gmail.com>
2024-05-23 17:51:04 +0200

    Création de la page d'erreur

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,10 @@
+import './index.scss';
+
+export default function Error() {
+	return (
+		<section className='error'>
+			<h2 className='error__title'>404</h2>
+			<p className='error__text'>Une erreur est survenu</p>
+		</section>
+	);
+}
