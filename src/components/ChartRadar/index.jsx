96dd0a410b0b4e64b2209f2c82943645c0093f3a
Camille Crapat <camille.capt@gmail.com>
2024-06-11 14:24:12 +0200

    Création de chartRadar

--- src/components/ChartRadar/index.jsx
+++ src/components/ChartRadar/index.jsx
@@ -1,5 +1,20 @@
+import { RadarChart, PolarGrid, PolarAngleAxis, Radar, ResponsiveContainer } from 'recharts';
+
 import './index.scss';
 
-export default function ChartRadar() {
-	return <div className='chartRadar'></div>;
+export default function ChartRadar({ data }) {
+	const kind = { 1: 'Intensité', 2: 'Vitesse', 3: 'Force', 4: 'Endurance', 5: 'Energie', 6: 'Cardio' };
+	const dataFormat = data.data.map((el, index) => ({ ...el, kind: kind[index + 1] }));
+
+	return (
+		<div className='chartRadar'>
+			<ResponsiveContainer width='100%' aspect={1}>
+				<RadarChart data={dataFormat} outerRadius='65%'>
+					<PolarGrid radialLines={false} />
+					<PolarAngleAxis style={{ fontSize: '12px' }} stroke='#FFFFFF' tickLine={false} dataKey='kind' />
+					<Radar dataKey='value' stroke='#FF0101B2' fill='#FF0101B2' fillOpacity={0.7} />
+				</RadarChart>
+			</ResponsiveContainer>
+		</div>
+	);
 }
