da648c1ed3da50d78d7b6e607ae6cbfa2a0fc3c9
Camille Crapat <camille.capt@gmail.com>
2024-06-18 12:49:59 +0200

    Création de chartStats

--- src/components/ChartStats/index.jsx
+++ src/components/ChartStats/index.jsx
@@ -1,5 +1,38 @@
-import './index.scss';
+import { ResponsiveContainer, RadialBarChart, RadialBar, Cell, Label } from 'recharts';
 
-export default function ChartStats() {
-	return <div className='chartStats'></div>;
+import './index.scss';
+import { useEffect } from 'react';
+
+export default function ChartStats({ data }) {
+	const score = [
+		{
+			type: 'max',
+			value: 1,
+		},
+		{
+			type: 'start',
+			value: !data.todayScore ? data.score : data.todayScore,
+		},
+	];
+	const background = ['#FFFFFF', '#E60000'];
+
+	return (
+		<div className='chartStats'>
+			<ResponsiveContainer width='100%' aspect={1}>
+				<span className='chartStats__title'>Score</span>
+				<div className='chartStats__obj'>
+					<p className='chartStats__obj--percent'>{score[1].value * 100 + '%'}</p>
+					<p className='chartStats__obj--text'>de votre</p>
+					<p className='chartStats__obj--text'>objectif</p>
+				</div>
+				<RadialBarChart data={score} innerRadius='53%' outerRadius='72%' startAngle={210} endAngle={-150}>
+					<RadialBar dataKey='value' cornerRadius={10}>
+						{score.map((entry, index) => (
+							<Cell key={`cell-${index}`} fill={background[index % background.length]} />
+						))}
+					</RadialBar>
+				</RadialBarChart>
+			</ResponsiveContainer>
+		</div>
+	);
 }
